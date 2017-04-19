08f253ea073f3028aa09de86dbf0d6400e37dfbe
dedekinds <469676727@qq.com>
2017-04-19 16:26:38 +0800

    Add files via upload

--- learningpython.py
+++ learningpython.py
@@ -804,10 +804,88 @@ L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
 
 L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
 def by_name(t):
-    return t[1]
-L2 = sorted(L, key=by_name,reverse=True)
+    return t[0]
+L2 = sorted(L, key=by_name)
 print(L2)
 
 
 习题2：
 再按成绩从高到低排序：
+L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+def by_name(t):
+    return t[1]
+L2 = sorted(L, key=by_name,reverse=True)
+print(L2)
+
+————————————————————————
+返回函数
+L=list(range(1,10))
+def lazy_sum(*args):
+    def sum():
+        ax = 0
+        for n in args:
+            ax = ax + n
+        return ax
+    return sum
+f=lazy_sum(*L)
+print(f())
+
+我们在函数lazy_sum中又定义了函数sum，并且，
+内部函数sum可以引用外部函数lazy_sum的参数和局部变量
+当lazy_sum返回函数sum时，相关参数和变量都保存在返回的函数中，
+这种称为“闭包（Closure）”的程序结构拥有极大的威力。
+
+
+闭包(Closure):内层函数引用了外层函数的变量(包括它的参数),
+然后返回内层函数的情况,这就是闭包.
+
+>>> f1 = lazy_sum(1, 3, 5, 7, 9)
+>>> f2 = lazy_sum(1, 3, 5, 7, 9)
+>>> f1==f2
+False
+即使输入相同的参数返回的函数都是不同的
+
+
+————————————————————————
+函数闭包
+def count():
+    fs = []
+    for i in range(1, 4):
+        def f():
+             return i*i
+        fs.append(f)
+    return fs
+f1, f2, f3 = count()
+print(f1())
+print(f2())
+print(f3())
+
+>>> 
+9
+9
+9
+结果全是9，这是因为原因就在于返回的函数引用了变量i，但它并非立刻执行。
+等到3个函数都返回时，它们所引用的变量i已经变成了3，因此最终结果为9。
+
+返回闭包时牢记的一点就是：
+**返回函数不要引用任何循环变量，或者后续会发生变化的变量。**
+
+解决方案：
+def count():
+    def f(j):
+        def g():
+            return j*j
+        return g
+    fs = []
+    for i in range(1, 4):
+        fs.append(f(i)) # f(i)立刻被执行，因此i的当前值被传入f()
+    return fs
+
+————————————————————————
+匿名函数
+list(map(lambda x: x * x, [1, 2, 3, 4, 5, 6, 7, 8, 9]))
+
+[x*x for x in range(1,10)]
+
+因为函数没有名字，不必担心函数名冲突:
+f=lambda x:x*x
