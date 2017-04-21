9d42a5b4f116a61528467ae9b652cf2400d7ef3a
dedekinds <469676727@qq.com>
2017-04-21 17:37:47 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,7 +1,23 @@
-def add_end(L=[]):
-    #L=t
-    L.append('END')
-    return L
-add_end()
-add_end()
-add_end()
+import functools
+def log(text):
+    if isinstance(text,str):#如果是字符串的话
+        def decorator(func):
+            @functools.wraps(func)
+            def wrapper(*args, **kw):
+                print('begin')
+                print('%s'%text)
+                func(*args,**kw)
+                print('end')
+            return wrapper
+        return decorator
+    else:
+            @functools.wraps(func)
+            def wrapper(*args, **kw):
+                print('begin')
+                func(*args,**kw)
+                print('end')
+            return wrapper
+@log('sdfdsf')
+def bar():
+    print('i am dedekinds')
+bar()
