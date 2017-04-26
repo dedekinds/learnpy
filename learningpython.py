640a012ac053cc7c331492f940b8ff5b0b0998ad
dedekinds <469676727@qq.com>
2017-04-26 15:28:27 +0800

    Add files via upload

--- learningpython.py
+++ learningpython.py
@@ -1050,4 +1050,27 @@ print(int2('124525'))
 43349
 >>> 
 
-回来学校之后就是不专心
+————————————————————————
+模块
+一个例子：
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+' a test module '
+#表示模块的文档注释，任何模块代码的第一个字符串都被视为模块的文档注释；
+
+__author__ = 'Michael Liao'
+
+import sys
+
+def test():
+    args = sys.argv
+    if len(args)==1:
+        print('Hello, world!')
+    elif len(args)==2:
+        print('Hello, %s!' % args[1])
+    else:
+        print('Too many arguments!')
+
+if __name__=='__main__':
+    test()
