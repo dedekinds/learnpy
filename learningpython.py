b9496f9bb9f447961f16b87921736abb258b87b4
dedekinds <469676727@qq.com>
2017-05-01 22:45:32 +0800

    Add files via upload

--- learningpython.py
+++ learningpython.py
@@ -1443,3 +1443,28 @@ print(len(dog))
 100
 >>> 
 优先输出。。诡异
+
+————————————————————————
+使用__slots__
+class Student(object):
+    __slots__ = ('name', 'age') # 用tuple定义允许绑定的属性名称
+>>> s = Student() # 创建新的实例
+>>> s.name = 'Michael' # 绑定属性'name'
+>>> s.age = 25 # 绑定属性'age'
+>>> s.score = 99 # 绑定属性'score'
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+AttributeError: 'Student' object has no attribute 'score'
+
+
+使用__slots__要注意，__slots__定义的属性仅对当前类实例起作用，
+对继承的子类是不起作用的：
+
+>>> class GraduateStudent(Student):
+...     pass
+...
+>>> g = GraduateStudent()
+>>> g.score = 9999
+
+
+————————————————————————
