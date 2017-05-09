c8eb7d3de3069ae4fa2cb02c1bf42b84da6a2d7c
dedekinds <469676727@qq.com>
2017-05-09 10:15:19 +0800

    Add files via upload

--- learningpython.py
+++ learningpython.py
@@ -1728,10 +1728,52 @@ f = open('test.txt', 'a')
 
 
 ————————————————————————
-StringIO和BytesIO
+StringIO和BytesIO（str数据
+
+StringIO → 内存中读写str
+
+from io import StringIO
+f=StringIO()
+f.write('hello')
+f.write('\n')
+f.write('dedekinds')
+print(f.getvalue())
+
+hello
+dedekinds
+>>> 
+
+from io import StringIO
+f = StringIO('Hello!\nHi!\nGoodbye!')
+while True:
+     s = f.readline()
+     if s == '':
+         break
+     print(s.strip())
+Hello!
+Hi!
+Goodbye!
+>>> 
+
+————————————————————————
+BytesIO（二进制数据
+
+>>> from io import BytesIO
+>>> f = BytesIO()
+>>> f.write('中文'.encode('utf-8'))
+6
+>>> print(f.getvalue())
+b'\xe4\xb8\xad\xe6\x96\x87'
 
 
+>>> from io import BytesIO
+>>> f = BytesIO(b'\xe4\xb8\xad\xe6\x96\x87')
+>>> f.read()
+b'\xe4\xb8\xad\xe6\x96\x87'
 
+
+————————————————————————
+操作文件和目录
 
 
 
