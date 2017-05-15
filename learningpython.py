8ad0ea2727f5354c5a99242b94be3cd9d9508ead
dedekinds <469676727@qq.com>
2017-05-15 15:56:50 +0800

    Add files via upload

--- learningpython.py
+++ learningpython.py
@@ -2067,4 +2067,89 @@ C ['C', 'C']
 A ['A', 'A', 'A']
 
 ————————————————————————
-contextlib
+contextlib（简化上下文管理）
+
+用with语句可以让f open 后及时被 close ，
+with open('/path/to/file', 'r') as f:
+    f.read()
+
+类似地也可以这样用with语句
+class Query(object):
+
+    def __init__(self, name):
+        self.name = name
+
+    def __enter__(self):
+        print('Begin')
+        return self#必须返回，没有的话query没有输入
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type:
+            print('Error')#__exit__的参数不懂？
+        else:
+            print('End')
+
+    def query(self):
+        print('Query info about %s...' % self.name)
+
+with Query('Bob') as q:
+    q.query()
+
+Begin
+Query info about Bob...
+End
+>>> 
+
+上面编写__enter__和__exit__仍然很繁琐
+
+from contextlib import contextmanager
+
+class Query(object):
+
+    def __init__(self, name):
+        self.name = name
+
+    def query(self):
+        print('Query info about %s...' % self.name)
+
+@contextmanager
+def create_query(name):
+    print('Begin')
+    q = Query(name)
+    yield q
+    print('End')
+
+with create_query('Bob') as q:
+    q.query()
+
+Begin
+Query info about Bob...
+End
+>>> 
+
+还能这么玩
+from contextlib import contextmanager
+@contextmanager
+def tag(name):
+    print("<%s>" % name)
+    yield
+    print("</%s>" % name)
+
+with tag("h1"):
+    print("hello")
+    print("world")
+
+<h1>
+hello
+world
+</h1>
+>>> 
+
+
+
+1.with语句首先执行yield之前的语句，因此打印出<h1>；
+2.yield调用会执行with语句内部的所有语句，因此打印出hello和world；
+3.最后执行yield之后的语句，打印出</h1>。
+
+————————————————————————
+urllib**
