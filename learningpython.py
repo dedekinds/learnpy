5d7d39251abb41029422cf7fb405813113f743ef
dedekinds <469676727@qq.com>
2017-05-15 11:05:59 +0800

    Add files via upload

--- learningpython.py
+++ learningpython.py
@@ -2006,3 +2006,65 @@ b5e2e2cc16973f6a35420e8b29ffe266
 但是运营商应该可以对用户的简单密码稍微添加一点字符，再md5即可
 
 
+————————————————————————
+itertools 迭代工具
+
+1.自然数序列无限：
+import itertools
+natuals=itertools.count(1)
+for n in natuals:
+    print(n)
+
+1
+2
+3
+4
+5
+>>> 
+
+2.字符串无限重复：
+>>> import itertools
+>>> cs = itertools.cycle('ABC') # 注意字符串也是序列的一种
+>>> for c in cs:
+...     print(c)
+...
+'A'
+'B'
+'C'
+'A'
+
+
+3.元素无限重复：
+>>> ns = itertools.repeat('A', 3)#第二个元素表示重复次数
+>>> for n in ns:
+...     print(n)
+...
+A
+A
+A
+
+
+除了用for做一个判断之外，还可以用takewhile()来截取无限数列
+>>> natuals = itertools.count(1)
+>>> ns = itertools.takewhile(lambda x: x <= 10, natuals)
+>>> list(ns)
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+特别地
+
+1.chain()可以将迭代对象组合起来，形成更大的迭代器：
+>>> for c in itertools.chain('ABC', 'XYZ'):
+...     print(c)
+# 迭代效果：'A' 'B' 'C' 'X' 'Y' 'Z'
+
+2.groupby()把迭代器中相邻的重复元素挑出来放在一起：
+>>> for key, group in itertools.groupby('AAABBBCCAAA'):
+...     print(key, list(group))
+...
+A ['A', 'A', 'A']
+B ['B', 'B', 'B']
+C ['C', 'C']
+A ['A', 'A', 'A']
+
+————————————————————————
+contextlib
