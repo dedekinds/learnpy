d641916120ade821b7bf63f1ffcef0946c596f99
dedekinds <469676727@qq.com>
2018-03-13 18:56:08 +0800

    Update tensorflow_movan.py

--- tensorflow_movan.py
+++ tensorflow_movan.py
@@ -315,7 +315,100 @@ with tf.Session() as sess:
             #*在测试的时候应该保持keep_prob为1，成为一个完整的网络
 
 
+#——————————————————————————————————————————————————————————
+CNN卷积神经网路
 
-        
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+
+
+def compute_accuracy(v_xs, v_ys):
+    global prediction
+    y_pre = sess.run(prediction, feed_dict={xs: v_xs, keep_prob: 1})
+    correct_prediction = tf.equal(tf.argmax(y_pre,1), tf.argmax(v_ys,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    result = sess.run(accuracy, feed_dict={xs: v_xs, ys: v_ys, keep_prob: 1})
+    return result
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape,mean=0.0,stddev=0.1)
+    return tf.Variable(initial)
+
+def bias_variable(shape):
+    initial = tf.constant(0.1,shape=shape)
+    return tf.Variable(initial)
+
+def conv2d(x, W):
+    return tf.nn.conv2d(x,W,strides=[1,1,1,1],padding='SAME')#*[1,x_movemont,y_movement,1,]
+    #'VALID':add notining
+    #'SAME':add 0
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
+    '''
+    (1)value：需要池化的输入，一般池化层接在卷积层后面，所以输入通常是feature map，依然是[batch, height, width, channels]这样的shape 
+    (2)ksize：池化窗口的大小，取一个四维向量，一般是[1, height, width, 1]，因为我们不想在batch和channels上做池化，所以这两个维度设为了1  
+    (3)strides：和卷积类似，窗口在每一个维度上滑动的步长，一般也是[1, stride,stride, 1]
+    (4)padding：和卷积类似，可以取'VALID' 或者'SAME'
+    (5)返回一个Tensor，类型不变，shape仍然是[batch, height, width, channels]这种形式
+    '''
+
+
+# define placeholder for inputs to network
+xs = tf.placeholder(tf.float32, [None, 784])#/255.? # 28x28
+ys = tf.placeholder(tf.float32, [None, 10])
+keep_prob = tf.placeholder(tf.float32)
+x_image = tf.reshape(xs,[-1,28,28,1])#*-1代表先不考虑输入的图片例子多少这个维度
+
+## conv1 layer ##
+W_conv1 = weight_variable([5,5,1,32])#patch 5*5   out:1channels depth=32?
+b_conv1 = bias_variable([32])
+h_conv1 = tf.nn.relu(conv2d(x_image,W_conv1)+b_conv1)#28*28*32
+h_pool1 = max_pool_2x2(h_conv1)#14*14*32
+
+
+
+## conv2 layer ##
+W_conv2 = weight_variable([5,5,32,64])
+b_conv2 = bias_variable([64])
+h_conv2 = tf.nn.relu(conv2d(h_pool1,W_conv2)+b_conv2)#14*14*64
+h_pool2 = max_pool_2x2(h_conv2)##7*7*64
+
+## func1 layer ##
+W_fc1 = weight_variable([7*7*64,1024]) 
+b_fc1 = bias_variable([1024])
+h_pool2_flat=tf.reshape(h_pool2,[-1,7*7*64]) #flat [n_samples,7,7,64]-->[n_sample,7*7*64]
+
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat,W_fc1)+b_fc1)
+h_fc1_drop=tf.nn.dropout(h_fc1,keep_prob)
+
+
+## func2 layer ##
+W_fc2 = weight_variable([1024,10]) 
+b_fc2 = bias_variable([10])
+prediction = tf.nn.softmax(tf.matmul(h_fc1_drop,W_fc2)+b_fc2)
+
+
+
+
+# the error between prediction and real data
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction),reduction_indices=[1]))       # loss
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)#AdamOptimizer
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+for i in range(2000):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys, keep_prob: 0.5})
+    if i % 50 == 0:
+        print(compute_accuracy(
+            mnist.test.images[:1000], mnist.test.labels[:1000]))
+
+
+
         
         
